67e8dd9eee507d55a902ee23fd3a664f15bfd4ee
Aine Forge <forge@aine.dev>
2026-07-02 17:20:32 +0000

    feat: Fix client-side exception in SessionProvider usage (Issue #46)
    Generated by Aine Forge
    
    Plan steps:
    1. [modify] Fix SessionProvider to use proper JSX component syntax instead of function call (files: pages/_app.tsx)
    2. [modify] Remove the unnecessary @typescript-eslint/no-explicit-any eslint-disable comment and any type cast (files: pages/_app.tsx)
    3. [validate] Run yarn lint to verify no linting errors are introduced (files: )
    4. [validate] Run yarn typecheck to verify TypeScript type correctness (files: )

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,19 @@
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider as NextAuthSessionProvider } from 'next-auth/react';
 import type { SessionProviderProps } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 import React from 'react';
 import './styles.css';
 
+// Wrapper to fix type compatibility with React 19
+const SessionProvider = NextAuthSessionProvider as React.FC<SessionProviderProps>;
+
 // Use the <SessionProvider> to improve performance and allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
 export default function App({ Component, pageProps }: AppProps) {
+  const { session, ...restPageProps } = pageProps;
   return (
-    <SessionProvider session={pageProps.session as any}>
-      <Component {...pageProps} />
+    <SessionProvider session={session}>
+      <Component {...restPageProps} />
     </SessionProvider>
   );
 }
