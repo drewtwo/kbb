9e2bbec42528a0e7d884f22056874563005d5537
Aine Forge <forge@aine.dev>
2026-07-02 17:18:05 +0000

    checkpoint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,14 @@
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
+import React from 'react';
 import './styles.css';
 
 // Use the <SessionProvider> to improve performance and allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (SessionProvider as any)({
-      session: pageProps.session,
-      children: <Component {...pageProps} />,
-    })
+  return React.createElement(
+    SessionProvider as React.ComponentType<any>,
+    { session: pageProps.session },
+    React.createElement(Component, pageProps)
   );
 }
